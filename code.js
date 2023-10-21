077815ac8da58635a65a0443e13b087936efa517
wizard <peterpapp@makingcg.com>
2023-10-21 16:33:21 +0200

    implemented new UI

--- code.js
+++ code.js
@@ -5,9 +5,11 @@ figma.showUI(__html__, { width: 400, height: 800 });
 figma.on('selectionchange', async () => {
     const selectedNodes = figma.currentPage.selection;
 
-    console.log(selectedNodes[0].type)
+    if (!selectedNodes[0]) {
+        figma.ui.postMessage({ type: 'clear-image' });
+    }
 
-    if (selectedNodes.length === 1 && ['FRAME', 'COMPONENT', 'INSTANCE'].includes(selectedNodes[0].type)) {
+    if (selectedNodes.length === 1 && ['FRAME', 'COMPONENT', 'INSTANCE', 'GROUP'].includes(selectedNodes[0].type)) {
         const frame = selectedNodes[0];
 
         const exportedFrame = await frame.exportAsync();
@@ -15,4 +17,28 @@ figma.on('selectionchange', async () => {
         figma.ui.postMessage({ type: 'convert-image', exportedFrame });
     }
 });
+
+// Retrieve message from UI
+figma.ui.onmessage = async (message) => {
+
+    // Set OpenAI token
+    if (message.type === 'set-openai-token') {
+        await figma.clientStorage.setAsync('openAIToken', message.data)
+    }
+}
+
+// Load OpenAI token from client storage
+figma.clientStorage.getAsync('openAIToken').then(openAIToken => {
+    figma.ui.postMessage({ type: 'get-openai-token', openAIToken });
+});
+
+// Get active user's avatar
+const activeUserAvatar = figma.currentUser.photoUrl;
+
+// Send active user's avatar to UI
+figma.ui.postMessage({ type: 'active-user-avatar', activeUserAvatar });
+
+
+
+
 
